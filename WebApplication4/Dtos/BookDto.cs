e9a17d2c7cd2a2296daa577f9d85d4865497729e
Samm8082070 <samm8082070@gmail.com>
2025-03-04 20:14:10 +0330

    feat: Add DTOs for book and genre data
    Created the following DTOs:
    - GenreDto: for transferring genre data.
    - BookEditDto: for editing book details, including genre selection.
    - BookDto: for displaying book details, including associated genres.
    - BookCreateDto: for creating new books, including genre selection.

---
+++ WebApplication4/Dtos/BookDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication4.Dtos
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public DateOnly PublishDate { get; set; }
+        public List<GenreDto> Genres { get; set; } = new List<GenreDto>(); // Nested Genre DTO
+    }
+}
---
+++ WebApplication4/Dtos/GenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication4.Dtos
+{
+    public class GenreDto
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+    }
+}
